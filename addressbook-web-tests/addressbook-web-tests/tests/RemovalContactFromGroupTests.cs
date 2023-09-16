9c3119717225fdbbc884ca8fda8c884d2c213ddc
khaybulaevan <naida-khaybulaeva@mail.ru>
2023-09-16 19:25:36 +0300

    Задание №17. Доработка теста TestRemovalContactFromGroup

--- addressbook-web-tests/addressbook-web-tests/tests/RemovalContactFromGroupTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/RemovalContactFromGroupTests.cs
@@ -14,18 +14,22 @@ namespace WebAddressbookTests
         {
             GroupData group = GroupData.GetAll()[0];
             List<ContactData> oldList = group.GetContacts();
-            ContactData contact = ContactData.GetAll().Except(oldList).First();
+
+            if (oldList.Count == 0)
+            {
+                app.Contacts.AddContactToGroup(ContactData.GetAll()[0], group);
+            }
+            ContactData contact = group.GetContacts()[0];
 
             app.Contacts.RemoveContactFromGroup(contact, group);
 
-
-            
             List<ContactData> newList = group.GetContacts();
             oldList.Remove(contact);
             newList.Sort();
             oldList.Sort();
 
             Assert.AreEqual(oldList, newList);
+        
         }
     }
 }
